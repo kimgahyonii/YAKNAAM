4ca08677909ec7754b2ab8d533eab70ef34a972a
wanghyunjooooo <w2324@e-mirim.hs.kr>
2024-11-03 22:30:03 +0900

    일단 커밋...

---
+++ js/login.js
@@ -0,0 +1,11 @@
+
+window.onload = function() {
+    const imageContainer = document.getElementById('imageContainer');
+    const mainContent = document.getElementById('mainContent');
+
+    // 5초 후에 imageContainer를 숨기고 mainContent를 표시
+    setTimeout(() => {
+        imageContainer.style.display = 'none';
+        mainContent.style.display = 'block';
+    }, 5000);
+};

8f96460c3047f7ad43108a35d22d30af9f9ff93d
김가현 <rkgus0324@gmail.com>
2024-11-03 22:52:54 +0900

    원본

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 
-window.onload = function() {
+window.onload = function () {
     const imageContainer = document.getElementById('imageContainer');
     const mainContent = document.getElementById('mainContent');
 
